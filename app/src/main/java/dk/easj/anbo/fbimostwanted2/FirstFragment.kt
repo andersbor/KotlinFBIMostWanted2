65e8da4c72875f7bfe296f030ea115e3f3879f4b
Anders Børjesson <anbo@zealand.dk>
2022-04-02 18:27:43 +0200

    Attempt to do swipe to goto next page (cannot do swipe on recyclerview)

--- app/src/main/java/dk/easj/anbo/fbimostwanted2/FirstFragment.kt
+++ app/src/main/java/dk/easj/anbo/fbimostwanted2/FirstFragment.kt
@@ -2,9 +2,8 @@ package dk.easj.anbo.fbimostwanted2
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -19,12 +18,21 @@ class FirstFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val catalogViewModel: CatalogViewModel by activityViewModels()
+    private lateinit var mDetector: GestureDetectorCompat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        mDetector = GestureDetectorCompat(activity, MyGestureListener())
+        val rootView = binding.root
+        rootView.setOnTouchListener { view, motionEvent ->
+            mDetector.onTouchEvent(motionEvent)
+            Log.d("APPLE", "Touch: " + motionEvent.x + " " + motionEvent.y)
+            true
+        }
         return binding.root
     }
 
@@ -68,5 +76,39 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private inner class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d("APPLE", "onLongPress $e")
+            super.onLongPress(e)
+        }
+
+        override fun onScroll(
+            ev1: MotionEvent, ev2: MotionEvent, distanceX: Float, distanceY: Float
+        ): Boolean {
+            Log.d("APPLE", "onScroll: $ev1 \n $ev2")
+            doIt(ev1, ev2)
+            return super.onScroll(ev1, ev2, distanceX, distanceY)
+        }
+
+        override fun onFling(
+            ev1: MotionEvent, ev2: MotionEvent, velocityX: Float, velocityY: Float
+        ): Boolean {
+            Log.d("APPLE", "onFling: $ev1 \n $ev2")
+            doIt(ev1, ev2)
+            return super.onFling(ev1, ev2, velocityX, velocityY)
+        }
+
+        private fun doIt(ev1: MotionEvent, ev2: MotionEvent) {
+            val xDiff = ev1.x - ev2.x
+            Log.d("APPLEPIE", "swipe $xDiff")
+            if (xDiff > 0) {
+                catalogViewModel.currentPage++ // TODO upper limit on currentPage
+                catalogViewModel.reload()
+            }
+        }
+
     }
 }
